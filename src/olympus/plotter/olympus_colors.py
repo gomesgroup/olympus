fc634286186dd4596a7afba59f257599c2fdabeb
rmacknight99 <rmacknight18@gmail.com>
2024-11-11 09:55:02 -0500

    updated for new python

--- src/olympus/plotter/olympus_colors.py
+++ src/olympus/plotter/olympus_colors.py
@@ -5,10 +5,12 @@
 # =====================
 
 import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib import colormaps
 from matplotlib.colors import LinearSegmentedColormap
+import numpy as np
 
-_olympus_reference_colors = [
+# Use numpy array for better performance
+_olympus_reference_colors = np.array([
     "#08294C",
     "#75BBE1",
     "#D4E9F4",
@@ -16,18 +18,29 @@ _olympus_reference_colors = [
     "#F7A4D4",
     "#F75BB6",
     "#EB0789",
-]
+])
+
+# Add N parameter for better color interpolation
 _olympus_cmap = LinearSegmentedColormap.from_list(
-    "olympus", _olympus_reference_colors
+    "olympus", _olympus_reference_colors, N=256
 )
 _olympus_cmap_r = LinearSegmentedColormap.from_list(
-    "olympus_r", _olympus_reference_colors[::-1]
+    "olympus_r", _olympus_reference_colors[::-1], N=256
 )
 
-plt.register_cmap(cmap=_olympus_cmap)
-plt.register_cmap(cmap=_olympus_cmap_r)
+# Register the colormaps using the new API
+colormaps.register(_olympus_cmap)
+colormaps.register(_olympus_cmap_r)
 
 
 def get_olympus_colors(n):
-    olympus_cmap = plt.get_cmap("olympus")
-    return [olympus_cmap(x) for x in np.linspace(0, 1, n)]
+    """Get n evenly spaced colors from the Olympus colormap.
+    
+    Args:
+        n (int): Number of colors to return
+        
+    Returns:
+        list: RGBA colors from the Olympus colormap
+    """
+    olympus_cmap = colormaps["olympus"]
+    return olympus_cmap(np.linspace(0, 1, n))
